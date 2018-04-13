9f9826b1b57af38aa23d606b453210187a788a60
TomMGrello <tommgrello@gmail.com>
2018-04-13 19:34:45 -0400

    First commit

---
+++ 2d Game/Assets/Scripts/TeamManager.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TeamManager : MonoBehaviour {
+	
+	//Team color constants
+	public const char TEAM_RED = 'r';
+	public const char TEAM_GREEN = 'g';
+	public const char TEAM_BLUE = 'b';
+	public const char TEAM_YELLOW = 'y';
+	public Dictionary<char, Color32> Starting_Colors = new Dictionary<char, Color32> ();
+	// Use this for initialization
+	void Start () {
+		Starting_Colors.Add(TEAM_RED, new Color32(255,0,0,255));
+		Starting_Colors.Add(TEAM_GREEN, new Color32(0,255,0,255));
+		Starting_Colors.Add(TEAM_BLUE, new Color32(0,0,255,255));
+		Starting_Colors.Add(TEAM_YELLOW, new Color32(255,255,0,255));
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ 2d Game/Assets/Scripts/player_manager.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class player_manager : MonoBehaviour {
+    /*******************************************************************************************************/
+    /********************************************* Properties **********************************************/
+    /*******************************************************************************************************/
+    public const int MAX_HEALTH = 100;
+    private SpriteRenderer spr;
+    private TeamManager tm;
+
+	//Health logic
+	private int m_Health = MAX_HEALTH;
+
+	public delegate void OnHealthChangeDelegate();
+	public event OnHealthChangeDelegate OnHealthChange;
+
+	public int Health {
+		get { return m_Health;}
+		set {
+			if (m_Health == value)
+				return;
+			m_Health = value;
+			if (OnHealthChange != null)
+				OnHealthChange ();
+		}
+	}
+
+	//Team logic
+	private char m_Team = 'n';
+
+	public delegate void OnTeamChangeDelegate();
+	public event OnTeamChangeDelegate OnTeamChange;
+
+	public char Team {
+		get { return m_Team;}
+		set {
+			if (m_Team == value)
+				return;
+			m_Team = value;
+			if (OnTeamChange != null)
+				OnTeamChange ();
+		}
+	}
+
+	/*******************************************************************************************************/
+	/********************************************** Functions **********************************************/
+	/*******************************************************************************************************/
+
+	void Dynamic_Color() {
+        Color32 Starting_Color = tm.Starting_Colors[m_Team];
+        int health_delta = MAX_HEALTH - m_Health;
+        int r = Starting_Color.r - health_delta < 0 ? 0 : Starting_Color.r - health_delta;
+        int g = Starting_Color.g - health_delta < 0 ? 0 : Starting_Color.g - health_delta;
+        int b = Starting_Color.b - health_delta < 0 ? 0 : Starting_Color.b - health_delta;
+        Color32 New_Color = new Color32((byte)r, (byte)g, (byte)b, 255);
+        spr.color = New_Color;
+    }
+
+	// Use this for initialization
+	void Start () {
+        tm = GameObject.FindGameObjectWithTag("Team Manager").GetComponent<TeamManager>();
+        spr = transform.Find("background").GetComponent<SpriteRenderer>();
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
